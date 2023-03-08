28b5baac5c794976b9d3d6d616ce37a66ddf0de5
ch_kai <ch_kaijack@outlook.com>
2023-03-08 09:28:29 +0800

    新建商品价格和库存和商品详情设置

--- src/components/goods/AddGood.vue
+++ src/components/goods/AddGood.vue
@@ -1,7 +1,37 @@
 <template>
-
+        <div class="content-container" direction="vertical">
+        <el-tabs v-model="activeTab" type="card" @tab-click="handleClick">
+        <el-tab-pane label="基础设置" name="1">
+            <base-setting></base-setting>
+        </el-tab-pane>
+        <el-tab-pane label="价格库存" name="2">
+            <price-setting></price-setting>
+        </el-tab-pane>
+        <el-tab-pane label="商品详情" name="3">
+            <detail-setting></detail-setting>
+        </el-tab-pane>
+  </el-tabs>
+    </div>
 </template>
 <script>
+import GoodBaseSetting from "@/components/goods/GoodBaseSetting.vue"
+import GoodDetailSetting from "@/components/goods/GoodDetailSetting.vue"
+import GoodPriceSetting from "@/components/goods/GoodPriceSetting.vue"
+export default {
+    data() {
+        return {
+            activeTab: "1"
+        }
+    },
+    updated() {
+        console.log("ch_kai:this is AddGood component updated data!",this.activeTab)
+    },
+    components: {
+        BaseSetting: GoodBaseSetting,
+        PriceSetting: GoodPriceSetting,
+        DetailSetting: GoodDetailSetting
+    }
+}
 </script>
 
 <style scoped></style>
